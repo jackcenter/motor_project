24dfe34d7ce77be02fd4ce288de74163567144b9
Jack <jack.d.center@gmail.com>
2025-12-13 22:01:57 -0700

    Add Blink Project

---
+++ firmware/blink/arm-gcc-toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(TOOLCHAIN_PREFIX arm-none-eabi)
+
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_OBJCOPY      ${TOOLCHAIN_PREFIX}-objcopy)
+set(CMAKE_SIZE         ${TOOLCHAIN_PREFIX}-size)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_FLAGS_INIT "-ffunction-sections -fdata-sections")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--gc-sections")
